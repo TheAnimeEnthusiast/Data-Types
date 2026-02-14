1109581ca24c33559798831d804c6186e6784c38
TheAnimeEnthusiast <mritvik02@gmail.com>
2026-02-13 20:35:54 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#Write a program to find what type of datatype the given variable is?
+
+
+dstring = "string"
+
+dinteger = 2
+
+dfloat = 2.22222222
+
+dboolean = True
+
+print(f"There are many types of data types. This one is {type(dstring)}")
+print(f"There are many types of data types. This one is {type(dinteger)}")
+print(f"There are many types of data types. This one is {type(dfloat)}")
+print(f"There are many types of data types. This one is {type(dboolean)}")
